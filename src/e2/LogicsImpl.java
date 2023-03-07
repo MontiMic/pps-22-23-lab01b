93066fa7fd7557adccc76e5e5210803a45505d5b
Michele <michele.monti7@studio.unibo.it>
2023-03-07 10:54:37 +0100

    Added random mines

--- src/e2/LogicsImpl.java
+++ src/e2/LogicsImpl.java
@@ -1,9 +1,21 @@
 package e2;
+import java.util.Random;
+import java.util.*;
 
 public class LogicsImpl implements Logics {
 
-    public LogicsImpl(int size) {
+    private final Set<Pair<Integer, Integer>> mines = new HashSet<>();
+    private final static int NUMBER_OF_MINES = 10;
 
+    public LogicsImpl(int size) {
+        Random random = new Random();
+        while (this.mines.size() <  NUMBER_OF_MINES){
+            this.mines.add(new Pair<>(random.nextInt(size), random.nextInt(size)));
+        }
     }
 
+    @Override
+    public Object getNumberOfMines() {
+        return this.mines.size();
+    }
 }
---
+++ src/test2/MinesweeperTests.java
@@ -0,0 +1,21 @@
+package test2;
+import e2.Logics;
+import e2.LogicsImpl;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinesweeperTests {
+
+    private Logics logic;
+    private final static int SIZE = 9;
+    private final static int MINES = 10;
+
+    @BeforeEach
+    void setUp(){
+        this.logic = new LogicsImpl(SIZE);
+    }
+    @Test
+    void testNumberOfMines(){
+        assertEquals(10, this.logic.getNumberOfMines());
+    }
+}
